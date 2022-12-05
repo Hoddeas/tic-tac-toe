423cea53cf8c7e7dd9e330283f2f60201b1d36e5
Hoddeas <fakehoddeas@gmail.com>
2022-12-05 13:27:48 -0700

    ui update

---
+++ main.js
@@ -0,0 +1,8 @@
+    
+function theme(mode) {
+    document.getElementById("game-container").setAttribute("display-mode", mode);
+    document.getElementById("gameboard").setAttribute("display-mode", mode);
+    for (let i = 0; i < 9; i++) {
+        document.getElementsByClassName("box")[i].setAttribute("display-mode", mode);
+    }
+}
