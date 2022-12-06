2c737ec7b065822b3bda6cdb26ac0147f4952904
Hoddeas <fakehoddeas@gmail.com>
2022-12-06 13:24:38 -0700

    small ui update

--- main.js
+++ main.js
@@ -2,7 +2,11 @@
 function theme(mode) {
     document.getElementById("game-container").setAttribute("display-mode", mode);
     document.getElementById("gameboard").setAttribute("display-mode", mode);
+    document.getElementById("header-container").setAttribute("display-mode", mode);
+    document.getElementById("title").setAttribute("display-mode", mode);
     for (let i = 0; i < 9; i++) {
         document.getElementsByClassName("box")[i].setAttribute("display-mode", mode);
     }
 }
+
+theme("dark")
