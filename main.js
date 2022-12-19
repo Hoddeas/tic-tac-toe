ed6da7ac4a8a088aea361baaf0a11c5d4af9ec3a
Hoddeas <fakehoddeas@gmail.com>
2022-12-19 13:27:25 -0700

    more work

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Tic-tac-toe
 
-// Function to change the theme of the webpage
+// Change Theme
 function theme(mode) {
     document.getElementById("game-container").setAttribute("data-display-mode", mode);
     document.getElementById("gameboard").setAttribute("data-display-mode", mode);
@@ -19,11 +19,11 @@ function theme(mode) {
     }
 }
 
-// Load saved theme
+// Load Theme
 let savedDisplayMode = localStorage.getItem("theme");
 theme(savedDisplayMode);
 
-// If first time loading page, then theme should be light
+// Default Theme = Light
 if (savedDisplayMode === null) {
     localStorage.setItem("theme", "light");
     theme("light");
@@ -33,7 +33,7 @@ if (savedDisplayMode === null) {
 let lightModeButton = document.getElementById("light-mode-button");
 let darkModeButton = document.getElementById("dark-mode-button");
 
-// Event listeners for theme button to change theme
+// Change theme
 lightModeButton.addEventListener("click", () => {
     theme("light");
     localStorage.setItem("theme", "light")
@@ -44,12 +44,75 @@ darkModeButton.addEventListener("click", () => {
     localStorage.setItem("theme", "dark")
 })
 
-// Event listener for clicked box and adding X or O to box
+// Player Mode
+let playerMode = "one-player";
+let player = document.getElementById("player");
+let playerOne = document.getElementById("player-1");
+let computer = document.getElementById("computer");
+let playerTwo = document.getElementById("player-2");
+let swapButton = document.getElementById("swap-buttons");
+
+swapButton.addEventListener("click", () => {
+    if (playerMode === "one-player") {
+        playerMode = "two-player";
+    } else if (playerMode === "two-player") {
+        playerMode = "one-player";
+    }
+
+    if (playerMode === "one-player") {
+        player.style.display = "inline-block";
+        computer.style.display = "inline-block";
+        playerOne.style.display = "none";
+        playerTwo.style.display = "none";
+    } else if (playerMode === "two-player") {
+        playerOne.style.display = "inline-block";
+        playerTwo.style.display = "inline-block";
+        player.style.display = "none";
+        computer.style.display = "none";
+    }
+});
+
+// Winning Combinations
+let win = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+];
+
+// Game
 let gameboard = document.getElementById("gameboard");
+let playerTurn = true;
+let computerTurn = false;
+let playerOneTurn = true;
+let playerTwoTurn = false;
+
 
 gameboard.addEventListener("click", (e) => {
-    e.target.classList.add("o")
-})
+    if (playerMode === "two-player") {
+        if (playerOneTurn) {
+            if (e.target.getAttribute("data-placed") === "o") {
+                return;
+            }
+            e.target.setAttribute("data-placed", "x");
+            playerOneTurn = false;
+            playerTwoTurn = true;
+        } else if (playerTwoTurn) {
+            if (e.target.getAttribute("data-placed") === "x") {
+                return;
+            }
+            e.target.setAttribute("data-placed", "o");
+            playerOneTurn = true;
+            playerTwoTurn = false;
+        }
+    }
 
-//
-
+    for (let i = 0; i < 8; i++) {
+        if (gameboard.children[win[i][0]].getAttribute("data-placed") === "x" && gameboard.children[win[i][1]].getAttribute("data-placed") === "x" && gameboard.children[win[i][2]].getAttribute("data-placed") === "x")
+            console.log("x win")
+    }
+});
