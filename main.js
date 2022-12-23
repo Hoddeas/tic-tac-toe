4ca8461f2345746584e096f20cec092c653e9808
Hoddeas <fakehoddeas@gmail.com>
2022-12-23 10:35:16 -0700

    Update main.js

--- main.js
+++ main.js
@@ -47,6 +47,13 @@ darkModeButton.addEventListener("click", () => {
 
 // Game
 
+// Board
+let board = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
 // Player Mode
 let playerMode = "one-player";
 let player = document.getElementById("player");
